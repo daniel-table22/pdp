c55f099bf9304bd6f4cf509032caab4d26190155
daniel-table22 <daniel.nacamuli@table22.com>
2025-05-22 11:52:04 -0700

    Connects ot strapi

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <button @click="fetchData">Fetch Strapi Data</button>
+    <pre v-if="result">{{ result }}</pre>
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const result = ref('')
+
+const fetchData = async () => {
+  result.value = 'Loading...'
+  try {
+    const res = await fetch('http://localhost:1337/api/businesses', {
+      headers: {
+        Authorization: 'Bearer c153ba748a898f1f8109eb9a6e9a409073c00e8a35ef9f7b063e7198b20e7ce77849ac188803e9df57750bcaf5728d1ef5138e3850d6b44100e67269897ad6aacd068be251c11c1db6445484dab0ead34bbe97bc7c40958ed69347742db7353d5536e0ea2b01290aa04a212549b4a1c270cdb479fb254bf09b855c8e22c06575'
+      }
+    })
+    const data = await res.json()
+    result.value = JSON.stringify(data, null, 2)
+  } catch (e) {
+    result.value = 'Error: ' + e.message
+  }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+button {
+  margin: 2rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+pre {
+  background: #f4f4f4;
+  padding: 1rem;
+  border-radius: 4px;
+  margin: 2rem;
 }
 </style>
